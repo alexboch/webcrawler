6b8a3908dcd27adc761572a6d89cf060c4231293
Alexander Bocharov <alexbocharov2@gmail.com>
2018-09-18 00:14:32 +0300

    Added crawler library

---
+++ CrawlerLib/Crawler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrawlerLib
+{
+    public class Crawler
+    {
+        #region Fields
+        private readonly CancellationTokenSource _cts=new CancellationTokenSource();
+#endregion
+
+        public void CancelCrawling()
+        {
+            _cts.Cancel();
+        }
+
+        private void CrawlDomain(string domain)
+        {
+            
+        }
+
+        private void CrawlPage()
+        {
+            
+        }
+
+        public async Task CrawlDomainsAsync(IEnumerable<string> domainNames,string outputPath)
+        {
+            
+            Parallel.ForEach(domainNames, (CrawlDomain));
+        }
+
+    }
+}

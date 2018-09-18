3568c9e1ca1cbe622b5a5086ff0c0c04188404f5
Alexander Bocharov <alexbocharov2@gmail.com>
2018-09-19 02:47:38 +0300

    main updated

--- CrawlerConsole/Program.cs
+++ CrawlerConsole/Program.cs
@@ -12,11 +12,7 @@ namespace CrawlerConsole
         static void Main(string[] args)
         {
             var crawler = new Crawler();
-            //crawler.CrawlDomainsAsync(new []{"https://google.ru/" },"Output").RunSynchronously();
-
-            var uri1 = new UriBuilder("google.yandex.com").Uri;
-            var subdomainUri = new UriBuilder("yandex.com").Uri;
-             subdomainUri.IsSubdomainOf(uri1);
+            crawler.CrawlDomainsAsync(new []{"https://google.ru/" },"Output");
             Console.ReadKey();
         }
     }
